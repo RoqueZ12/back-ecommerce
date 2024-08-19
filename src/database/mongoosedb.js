02a3c77cb33db656edd6d18656eed2f5b42bc3ef
santino <ing.roquezegarra@gmail.com>
2024-08-19 13:31:00 -0500

    version5

--- src/database/mongoosedb.js
+++ src/database/mongoosedb.js
@@ -1,16 +1,11 @@
-import mongoose from 'mongoose';
-import { DB } from '../env/env.js';
-
-export const mongoosedb = async () => {
+import mongoose from "mongoose";
+import { DB } from "../env/env.js";
+export const mongoosedb = async () => {  
   try {
-    // Conectar a la base de datos MongoDB
-    await mongoose.connect(DB, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
+    await mongoose.connect(DB);
+    console.log("MongoDB connected");
   } catch (error) {
-    // Manejar errores de conexión
-    console.error('Error connecting to MongoDB:', error);
+    console.log(error);
   }
-};
+} 
+
