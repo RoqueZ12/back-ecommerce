8714fe538fb00890d56c456a39cd8a8818000d1a
santino <ing.roquezegarra@gmail.com>
2024-08-19 13:29:15 -0500

    version 4

--- src/env/env.js
+++ src/env/env.js
@@ -1,3 +1,3 @@
 export const PORT = process.env.PORT ?? 1300
-export const DB = process.env.DB ?? 'mongodb+srv://juniorrxque:MHE0M2SVQmx8iRzS@examplecommerce.su5fc.mongodb.net/'
+export const DB = process.env.DB ?? 'mongodb://localhost:27017/User_Employee'
 export const SECRETKEY = process.env.SECRETKEY ?? 'secretkeymongodb'
